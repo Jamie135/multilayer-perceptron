bc42e222f53ce5ffc1361bd34cb6afeeb19060ce
Jamie <pomprab.b@gmail.com>
2024-12-10 18:31:30 +0700

    implemented dataset split program

---
+++ split.py
@@ -0,0 +1,26 @@
+import sys
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def main():
+    """split a dataset into training and test sets"""
+    if len(sys.argv) != 2:
+        print('Usage: python3 split.py data.csv')
+        sys.exit(1)
+
+    file = sys.argv[1]
+    try:
+        df = pd.read_csv(file)
+    except FileNotFoundError:
+        print(f'Error: {file} not found')
+        sys.exit(1)
+
+    train, test = train_test_split(df, test_size=0.2)
+
+    train.to_csv('train.csv', index=False)
+    test.to_csv('test.csv', index=False)
+
+
+if __name__ == '__main__':
+    main()
